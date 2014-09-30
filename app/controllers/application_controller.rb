5195d8de680c4c3d32652ce5a1a4e932911122b2
Hunter Husar <hunter@hunterhusar.net>
2014-09-30 11:40:32 -0500

    init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by using :null_session
+  protect_from_forgery with: :null_session
   include DeviseTokenAuth::Concerns::SetUserByToken
   include Pundit
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  respond_to :json
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :nickname
+    devise_parameter_sanitizer.for(:account_update) << :nickname
+  end
 end
