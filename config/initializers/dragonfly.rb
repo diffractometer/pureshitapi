aafb2f4307d44658a4eb46c6d6887d9814ae0009
Hunter Husar <hunter@hunterhusar.net>
2014-10-21 17:20:01 -0500

    buccked mess

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -14,10 +14,12 @@ Dragonfly.app.configure do
     # server_root: Rails.root.join('public')
   
   datastore :s3,
-    bucket_name: 'pureshit-production-uploads',
-    access_key_id: Rails.application.secrets.aws_key,
+    bucket_name:       'hunterhusar-production',
+    access_key_id:     Rails.application.secrets.aws_key,
     secret_access_key: Rails.application.secrets.aws_secret,
-    url_scheme: 'https'
+    url_scheme:        'https',
+    server_root:       'uploads',
+    root_path:         'uploads'
 end
 
 # Logger
