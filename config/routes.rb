ed7a465b5e0e87637cc650984fb177f90530aba0
Hunter Husar <hunter@hunterhusar.net>
2014-10-21 16:38:03 -0500

    hello world

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   # resources :events
   resources :users
 
+  root :to => "pages#hello"
+
   mount_devise_token_auth_for 'User', at: '/auth'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
