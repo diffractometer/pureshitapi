6328be71f42510c36bef6d88ec86b261bde8bb45
Hunter Husar <hunter@hunterhusar.net>
2014-10-24 17:41:31 -0500

    init

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,4 +1,11 @@
 class Attachment < ActiveRecord::Base
   dragonfly_accessor :file
   belongs_to :resource, polymorphic: true
+  validate :resource_limit
+
+  def resource_limit
+    if self.resource.attachments.length > 5
+      errors.add(:attachments_limit, "can't have more than 5 attachments")
+    end
+  end
 end
