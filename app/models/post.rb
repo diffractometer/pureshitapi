96367438ceae7e46ede5757c84dd2d392a99d34a
Hunter Husar <hunter@hunterhusar.net>
2014-10-24 17:13:25 -0500

    wip

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,9 @@ class Post < ActiveRecord::Base
   dragonfly_accessor :image   # defines a reader/writer for image
   belongs_to :user
   has_many :attachments, as: :resource
-
+  validates :title, presence: true
+  validate :attachments_count
+ 
   # scopes
   scope :for_index, -> { self
     .includes(:user,:attachments)
@@ -24,6 +26,12 @@ class Post < ActiveRecord::Base
 
   def attachments_attributes
     self.attachments.as_json
+  end
+
+  def attachments_count
+    if self.attachments.length > 5
+      errors.add(:attachments_limit, "can't have more than 5 attachments")
+    end
   end
 
 
